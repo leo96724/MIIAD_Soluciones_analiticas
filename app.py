8da8a8dbba684c46e52dd4aa2aba03f09405bddf
leo96724 <leoalv96@gmal.com>
2025-10-12 04:38:52 +0000

    exponer app en 0.0.0.0:8050 para despliegue

--- app.py
+++ app.py
@@ -274,6 +274,9 @@ def update_output_div(date, hour, proy):
 # Run the server
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
+<<<<<<< HEAD
 
 #ajuste de texto en descripción del tablero guia 2 parte 2
 #ajuste de texto en descripción del tablero guia 2 parte 3
+=======
+>>>>>>> 8610685 (exponer app en 0.0.0.0:8050 para despliegue)
